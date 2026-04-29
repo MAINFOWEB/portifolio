e492781509fb9ca914be0078c19facf321a6e9a4
MAINFOWEB <165689605+MAINFOWEB@users.noreply.github.com>
2026-04-29 17:11:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -55,3 +55,16 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
+const btn = document.getElementById("backToTop");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+btn.onclick = function() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+};
